5898c19daac6b33d961179f9e6e566d525d0aec5
PhoneMinMyat <phoneminmyat1999@gmail.com>
2023-10-21 20:31:49 +0630

    add logo and change name

--- lib/pages/dream_list_page.dart
+++ lib/pages/dream_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:dream_dictionary/bloc/dream_list_bloc.dart';
+import 'package:dream_dictionary/constants/colors.dart';
 import 'package:dream_dictionary/constants/dimens.dart';
 import 'package:dream_dictionary/widgets/background.dart';
 import 'package:dream_dictionary/widgets/dream_item.dart';
@@ -21,23 +22,49 @@ class DreamListPage extends StatelessWidget {
             Column(
               children: [
                 Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: MARGIN_MEDIUM_2x, vertical: MARGIN_LARGE),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: bloc.dreamList?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        return Padding(
+                  child: (bloc.dreamList?.isEmpty ?? false)
+                      ? Center(
+                          child: Container(
+                            padding: const EdgeInsets.all(MARGIN_XLARGE),
+                            decoration: const BoxDecoration(
+                                color: Colors.white, shape: BoxShape.circle),
+                            child: const Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  Icons.search_off,
+                                  color: AUTOCOMPLETE_OPTIONS_COLOR,
+                                  size: MARGIN_XLARGE,
+                                ),
+                                Text(
+                                  'No Dream\nFound',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: AUTOCOMPLETE_OPTIONS_COLOR),
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      : Padding(
                           padding: const EdgeInsets.symmetric(
-                              vertical: MARGIN_SMALL),
-                          child: DreamItem(
-                              text: bloc.dreamList?[index].blogContent ?? '',textAlign: TextAlign.left,
-                              onTap: () {}),
-                        );
-                      },
-                    ),
-                  ),
+                              horizontal: MARGIN_MEDIUM_2x,
+                              vertical: MARGIN_LARGE),
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: bloc.dreamList?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: MARGIN_SMALL),
+                                child: DreamItem(
+                                    text: bloc.dreamList?[index].blogContent ??
+                                        '',
+                                    textAlign: TextAlign.left,
+                                    onTap: () {}),
+                              );
+                            },
+                          ),
+                        ),
                 )
               ],
             )
